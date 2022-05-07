a60b581c5579155a12f6831c381f44381686d53a
eraymond1 <ericraymond@rocketmail.com>
2022-05-07 10:09:25 -0400

    adding js code to display content on page

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,7 +9,7 @@ var apiAlerts = "https://api.weatherbit.io/v2.0/alerts?postal_code=06282&key=633
 
 //three fecth commands to handle the three urls. Will be adding NOAA api soon.
 var currentWeather;
-<<<<<<< HEAD
+
 fetch(apiCurrent, {
     method: "POST", body: JSON.stringify(currentWeather)
 }).then(response => response.json()).then(json => console.log(json));
@@ -19,14 +19,13 @@ fetch(apiCurrent, {
     method: "POST", body: JSON.stringify(airQual)
 }).then(response => response.json()).then(json => console.log(json));
 
-=======
+
 // fetch(apiCurrent, {
 //     method: "POST", body: JSON.stringify(currentWeather)
 // }).then(response => response.json()).then(json => console.log(json));
 
 function fetchData() {
 
-<<<<<<< HEAD
     fetch(apiCurrent).then(response => {
         // console.log(response);
         if(!response.ok){
@@ -73,15 +72,10 @@ fetchData();
 // fetch(apiCurrent, {
 //     method: "POST", body: JSON.stringify(weatherAlerts)
 // }).then(response => response.json()).then(json => console.log(json));
-=======
->>>>>>> f89ff5a47d8b8e2b5a09458f5abd1ebd3b213e6e
 var weatherAlerts;
 fetch(apiCurrent, {
     method: "POST", body: JSON.stringify(weatherAlerts)
 }).then(response => response.json()).then(json => console.log(json));
-<<<<<<< HEAD
-=======
 
 
->>>>>>> f53da83f8072c961b24a9f128bafb0b68beddf12
->>>>>>> f89ff5a47d8b8e2b5a09458f5abd1ebd3b213e6e
+
