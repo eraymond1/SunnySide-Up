a32fb9367c2fe277b57ae1030269527b1ada0c71
eraymond1 <ericraymond@rocketmail.com>
2022-05-08 10:51:06 -0400

    fixed form and api url so now zip is taken from search window, created new function

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,6 @@
 
 //apiCurrent supplys all the current weather information needed
-var apiCurrent = "https://api.weatherbit.io/v2.0/current?postal_code=06282&key=633d8e359dcd43f1afbf0304c7e3bd28";
+//var apiCurrent = "https://api.weatherbit.io/v2.0/current?postal_code=06282&key=23f4eb9104a3417ebae0fd654b5b8faa";
 
 //var apiAirQual = "https://api.weatherbit.io/v2.0/airquality?postal_code=06282&key=633d8e359dcd43f1afbf0304c7e3bd28";
 
@@ -8,35 +8,86 @@ var apiCurrent = "https://api.weatherbit.io/v2.0/current?postal_code=06282&key=6
 
 
 //fetchData currently only fetching weatherbit io 
-function fetchData() {
+// function fetchData() {
 
-    fetch(apiCurrent).then(response => {
-        // console.log(response);
-        if(!response.ok){
-            throw Error("ERROR");
-        }
-        return response.json();
-    }).then(data => {
+//     fetch(apiCurrent).then(response => {
+//         // console.log(response);
+//         if(!response.ok){
+//             throw Error("ERROR");
+//         }
+//         return response.json();
+//     }).then(data => {
 
-        //using map on all variables to collect api data and parse it to display on webpage
+//         //using map on all variables to collect api data and parse it to display on webpage
+//         var airTemp = data.data.map(current => {
+//             return ` ${current.app_temp}`;
+//         }).join("");
+//         document.querySelector("#air-temp").insertAdjacentHTML("afterbegin","Air Temperature: " + ((airTemp * 1.8) + 32));
+
+        
+//         var weatherCond = data.data.map(current => {
+//             return `${current.weather.description}`;            
+//         }).join("");
+//         console.log(data.data);        
+//         document.querySelector("#weather-condition").insertAdjacentHTML("afterbegin", "Weather Conditions: " + weatherCond);
+
+
+//         var windSpeed = data.data.map(current => {
+//             return `${current.wind_spd}`;
+//         }).join("");
+//         document.querySelector("#wind-speed").insertAdjacentHTML("afterbegin", "Wind Speed: " + windSpeed);
+
+
+//         var uvIndex = data.data.map(current => {
+//             return `${current.uv}`;
+//         }).join("");
+//         document.querySelector("#UV-index").insertAdjacentHTML("afterbegin", "UV Index: " + uvIndex);
+
+//         var airQuality = data.data.map(current => {
+//             return `${current.aqi}`;
+//         }).join("");
+//         document.querySelector("#pollution").insertAdjacentHTML("afterbegin", "Air Quality: " + airQuality);
+        
+//     }).catch(error => {
+//         console.log(error);  
+//     });
+
+// }
+
+// fetchData();
+
+
+
+//getZipCode();
+
+document.querySelector('#search-form').addEventListener
+('submit', getZipCode);
+
+function getZipCode(event){
+    //get zip from input
+    var zip = document.querySelector('.input').value;
+    console.log(zip);
+    //make request
+    fetch("https://api.weatherbit.io/v2.0/current?postal_code=" + zip + "&key=23f4eb9104a3417ebae0fd654b5b8faa")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
         var airTemp = data.data.map(current => {
             return ` ${current.app_temp}`;
         }).join("");
         document.querySelector("#air-temp").insertAdjacentHTML("afterbegin","Air Temperature: " + ((airTemp * 1.8) + 32));
 
-        
         var weatherCond = data.data.map(current => {
             return `${current.weather.description}`;            
         }).join("");
         console.log(data.data);        
         document.querySelector("#weather-condition").insertAdjacentHTML("afterbegin", "Weather Conditions: " + weatherCond);
 
-
         var windSpeed = data.data.map(current => {
             return `${current.wind_spd}`;
         }).join("");
         document.querySelector("#wind-speed").insertAdjacentHTML("afterbegin", "Wind Speed: " + windSpeed);
-
 
         var uvIndex = data.data.map(current => {
             return `${current.uv}`;
@@ -47,14 +98,16 @@ function fetchData() {
             return `${current.aqi}`;
         }).join("");
         document.querySelector("#pollution").insertAdjacentHTML("afterbegin", "Air Quality: " + airQuality);
-        
-    }).catch(error => {
-        console.log(error);  
+
     });
 
+    
+    event.preventDefault();
+    
+   return zip;
+     
 }
 
-fetchData();
 
 
 
