6c75ebaaff45832b23cefb216a6ff2d9d8c4136e
eraymond1 <ericraymond@rocketmail.com>
2022-05-09 16:20:52 -0400

    added alerts to alert section to display alerts for area searched or to display great day for sailing if no alerts

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,59 +4,7 @@
 
 //var apiAirQual = "https://api.weatherbit.io/v2.0/airquality?postal_code=06282&key=633d8e359dcd43f1afbf0304c7e3bd28";
 
-//var apiAlerts = "https://api.weatherbit.io/v2.0/alerts?postal_code=06282&key=633d8e359dcd43f1afbf0304c7e3bd28";
-
-
-//fetchData currently only fetching weatherbit io 
-// function fetchData() {
-
-//     fetch(apiCurrent).then(response => {
-//         // console.log(response);
-//         if(!response.ok){
-//             throw Error("ERROR");
-//         }
-//         return response.json();
-//     }).then(data => {
-
-//         //using map on all variables to collect api data and parse it to display on webpage
-//         var airTemp = data.data.map(current => {
-//             return ` ${current.app_temp}`;
-//         }).join("");
-//         document.querySelector("#air-temp").insertAdjacentHTML("afterbegin","Air Temperature: " + ((airTemp * 1.8) + 32));
-
-        
-//         var weatherCond = data.data.map(current => {
-//             return `${current.weather.description}`;            
-//         }).join("");
-//         console.log(data.data);        
-//         document.querySelector("#weather-condition").insertAdjacentHTML("afterbegin", "Weather Conditions: " + weatherCond);
-
-
-//         var windSpeed = data.data.map(current => {
-//             return `${current.wind_spd}`;
-//         }).join("");
-//         document.querySelector("#wind-speed").insertAdjacentHTML("afterbegin", "Wind Speed: " + windSpeed);
-
-
-//         var uvIndex = data.data.map(current => {
-//             return `${current.uv}`;
-//         }).join("");
-//         document.querySelector("#UV-index").insertAdjacentHTML("afterbegin", "UV Index: " + uvIndex);
-
-//         var airQuality = data.data.map(current => {
-//             return `${current.aqi}`;
-//         }).join("");
-//         document.querySelector("#pollution").insertAdjacentHTML("afterbegin", "Air Quality: " + airQuality);
-        
-//     }).catch(error => {
-//         console.log(error);  
-//     });
-
-// }
-
-// fetchData();
-
-
+//var apiAlerts = "https://api.weatherbit.io/v2.0/alerts?postal_code=90210&key=23f4eb9104a3417ebae0fd654b5b8faa";
 
 document.querySelector('#search-form').addEventListener
     ('submit', getZipCode);
@@ -74,7 +22,7 @@ function getZipCode(event){
     .then(response => response.json())
     .then(data => {
 
-
+        console.log(data);
         var airTemp = data.data.map(current => {
             return ` ${current.app_temp}`;
         }).join("");
@@ -83,7 +31,7 @@ function getZipCode(event){
         var weatherCond = data.data.map(current => {
             return `${current.weather.description}`;            
         }).join("");
-        console.log(data.data);        
+       // console.log(data.data);        
         document.querySelector("#weather-condition").insertAdjacentHTML("afterbegin", "Weather Conditions: " + weatherCond);
 
         var windSpeed = data.data.map(current => {
@@ -106,9 +54,40 @@ function getZipCode(event){
         }).join("");
         document.querySelector(".city-name").insertAdjacentText("afterbegin","Current weather for "  + cityName);
 
-       console.log(cityName);
+       //console.log(cityName);
         
 
+    });
+
+    fetch("https://api.weatherbit.io/v2.0/alerts?postal_code=" + zip + "&key=23f4eb9104a3417ebae0fd654b5b8faa")
+    .then(response => {
+        if(!response.ok) {
+            throw Error("ERROR");
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+
+        var weatherAlerts = data.alerts
+        .map(alerts => {
+            return `<p> ${alerts.description}</p>`;
+        })
+        .join("");
+
+        if(Object.keys(data.alerts).length) {
+            document.querySelector(".alert-text").insertAdjacentHTML("afterbegin", "Current Alerts: " + weatherAlerts);
+            
+        }else {
+            weatherAlerts = "Great Day for Sailing!";
+            document.querySelector(".alert-text").insertAdjacentHTML("afterbegin", weatherAlerts);
+            
+        }
+        
+    })
+    .catch(error => {
+        console.log(error);
     });
 
     var submitBtn = document.querySelector("#search-form");
@@ -127,26 +106,6 @@ function getZipCode(event){
   
      
 }
-
-
-
-//clearForm();
-
-
-
-// var airQual;
-// fetch(apiCurrent, {
-//     method: "POST", body: JSON.stringify(airQual)
-// }).then(response => response.json()).then(json => console.log(json));
-
-// var weatherAlerts;
-// fetch(apiCurrent, {
-//     method: "POST", body: JSON.stringify(weatherAlerts)
-// }).then(response => response.json()).then(json => console.log(json));
-//var weatherAlerts;
-// fetch(apiCurrent, {
-//     method: "POST", body: JSON.stringify(weatherAlerts)
-// }).then(response => response.json()).then(json => console.log(json));
 
 
 
